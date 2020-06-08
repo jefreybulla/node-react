b5052d03d06b95dc50bb4412a9f1969e0f955919
jefreybulla <jefrey.bulla@gmail.com>
2020-06-08 19:31:54 -0400

    edit API source

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import HandleCookies from './HandleCookies';
 
-const APIGet = "https://rails-auth-2020.herokuapp.com/data"
+// const APIGet = "https://rails-auth-2020.herokuapp.com/data"
+const APIGet = "http://rails.toro-labs.com/data"
 // const APIGet = "https://s1.ngrok.io/data"
 
 class App extends React.Component {
