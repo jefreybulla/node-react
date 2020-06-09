eb23a56cec2c2e9ee6f167dd29879999bc877029
jefreybulla <jefrey.bulla@gmail.com>
2020-06-09 11:22:27 -0400

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import HandleCookies from './HandleCookies';
 
-// const APIGet = "https://rails-auth-2020.herokuapp.com/data"
 const APIGet = "http://rails.toro-labs.com/data"
 // const APIGet = "https://s1.ngrok.io/data"
 
